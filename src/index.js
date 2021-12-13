949700d6f43066e77b61ab1b39c48bd7479fe4ca
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2021-12-12 21:20:54 -0300

    Finishing challenger

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+    const { username } = request.headers;
+
+    const user = users.find(u => u.username === username);
+
+    if(!user) {
+      return response.status(404).json({ error: "User not found" })
+    }
+
+    request.user = user;
+    return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro) {
+    return next();
+  }
+
+  if(user.todos.length >= 10) {
+    return response.status(403).json({ error: "You don't have todo available" })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({ error: "id is not valid"})
+  }
+
+  const todo = user.todos.find(t => t.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "todo not found"})
+  }
+
+  request.todo = todo;
+  request.user = user;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "user not found"})
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
